2785bc387dddbb53701a18733ec08f8374c3ca41
Crhispo <cristianandrade2001@gmail.com>
2022-03-22 14:06:54 -0500

    Se añadieron nuevas vistas

--- Projecto_DJ_uno/views.py
+++ Projecto_DJ_uno/views.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from django.template import Template, Context
 from django.http import HttpResponse
 
 
@@ -38,5 +39,56 @@ def ageCategory(request, age):  # Argumento de tipo "Request", parametro edad
 
 
 def getCurrentdate(request):  # Argumento de tipo "Request"
-    Response = "<H1>Momento actual: {0} </H1>".format(datetime.now().strftime("%A %d/%m%Y %H:%M:%S"))
+    Response = "<H1>Momento actual: {0} </H1>".format(
+        datetime.now().strftime("%A %d/%m/%Y %H:%M:%S"))
     return HttpResponse("<H1>El servidor responde que: %s </H1>" % Response)
+
+
+def contentHTML(request, name, age):  # Argumento de tipo "Request"
+    content = """
+    <html>
+    <head>
+    <title>
+    Contenido HTML
+    </title>
+    </head>
+    <body>
+    <p>Nombre: %s / Edad: %s </p>
+    </body>
+    </html>
+    """ % (name, age)
+    return HttpResponse(content)
+
+
+def firtstemplate(request):  # Argumento de tipo "Request"
+    # Abrimos el documento que contiene la plantilla
+    temexterna = open(
+        '../Projecto_DJ_uno/Projecto_DJ_uno/plantillas/firtsTem.html')
+    # Se carga el documento en una variable tipo "template":
+    template = Template(temexterna.read())
+    # Cerrar el documento externo que hemos abierto
+    temexterna.close()
+    # Crear el contexto:
+    context = Context()
+    # Renderizar el documento
+    document = template.render(context)
+    return HttpResponse(document)
+
+
+def templateParameters(request):  # Argumento de tipo "Request"
+    nombre = "Crhistian"
+    fechaActual = datetime.now().strftime("%A %d/%m/%Y %H:%M:%S")
+    lenguajes = ["python", "Golang", "JavaScript", "C+", "Ruby"]
+    # Abrimos el documento que contiene la plantilla
+    temexterna = open(
+        '../Projecto_DJ_uno/Projecto_DJ_uno/plantillas/temParameters.html')
+    # Se carga el documento en una variable tipo "template":
+    template = Template(temexterna.read())
+    # Cerrar el documento externo que hemos abierto
+    temexterna.close()
+    # Crear el contexto:
+    context = Context(
+        {"nombre": nombre, "fechaActual": fechaActual, "lenguajes": lenguajes})
+    # Renderizar el documento
+    document = template.render(context)
+    return HttpResponse(document)
